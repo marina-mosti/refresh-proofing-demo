4c7b5566ff9d12792ac3f8cfb77181cd435756ae
Marina Mosti <marina@Marinas-MacBook-Pro.local>
2019-11-04 22:07:34 -0600

    Remove example code for basic starter

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
     <img alt="Vue logo" src="./assets/logo.png">
 
     <form @submit.prevent="submit">
-      <BaseInput label="First name" name="firstName" :value="form.firstName" @input="updateForm('firstName', $event)" />
-      <BaseInput label="Last name" name="lastName" :value="form.lastName" @input="updateForm('lastName', $event)" />
-      <BaseInput label="Nickname" name="nickname" :value="form.nickname" @input="updateForm('nickname', $event)" />
-      <BaseInput label="Age" name="age" type="number" :value="form.age" @input="updateForm('age', $event)" />
-      <BaseInput label="CC Number" name="cc" :value="form.cc" @input="updateForm('cc', $event)" v-mask="'####-####-####-####'" />
+      <BaseInput label="First name" name="firstName" />
+      <BaseInput label="Last name" name="lastName" />
+      <BaseInput label="Nickname" name="nickname" />
+      <BaseInput label="Age" name="age" type="number" />
+      <BaseInput label="CC Number" name="cc" />
 
-      <BaseSelect label="Shirt size" name="shirtSize" :options="shirts" :value="form.shirtSize" @input="updateForm('shirtSize', $event)" />
-      <BaseSelect label="Favorite noms" name="noms" :options="noms" :value="form.noms" @input="updateForm('noms', $event)" />
+      <BaseSelect label="Shirt size" name="shirtSize" :options="shirts" />
+      <BaseSelect label="Favorite noms" name="noms" :options="noms" />
       <button type="submit">SEND!</button>
     </form>
   </div>
@@ -46,16 +46,6 @@ export default {
   methods: {
     submit () {
       console.log('Sent')
-    },
-    updateForm (param, value) {
-      this.form[param] = value
-      localStorage.setItem('form', JSON.stringify(this.form))
-    }
-  },
-  created () {
-    const form = localStorage.getItem('form')
-    if (form) {
-      this.form = JSON.parse(form)
     }
   }
 }
